333ea2f7ec084aa2939967bfae6cb8da86336c34
francesca-campana <franx3rox@gmail.com>
2020-07-07 17:03:23 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
 $(document).ready(function(){
-  $('button').click(function(){
+  getTodo()
+  function getTodo(){
+    $.ajax(
+
+      {
+        url: 'http://157.230.17.132:3003/todos/',
+        method: 'GET',
+        success: function(dataRes){
+          if (dataRes.length > 0) {
+
+            var source = $("#list-template").html();
+            var template = Handlebars.compile(source);
+            for (var i = 0; i < dataRes.length; i++) {
+              var todoItem = dataRes[i];
+              var html = template(todoItem);
+              $('#todo-list').append(html);
+            }
+          }
+        },
+        error: function(){
+          alert('La tua richiesta non ha portato alcun risultato');
+        }
+
+      }
+    );
+  };
+
+  $('#add-todo').click(function(){
+
     var newTodoText = $('input').val();
+
+    
+
 
   });
 
