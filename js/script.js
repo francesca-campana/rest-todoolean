5eaa1e073c5502cf9d70210826906c2f4253f4a0
francesca-campana <franx3rox@gmail.com>
2020-07-07 17:56:41 +0200

    mostra e inserisci elementi nella lista

--- js/script.js
+++ js/script.js
@@ -1,6 +1,52 @@
 $(document).ready(function(){
-  getTodo()
-  function getTodo(){
+   getTodoTodo();
+
+  // Clicca e inserisci, il valore inserito nella input 'newTodoText',
+  // nella lista 'todo-list'
+
+  $('#add-todo').click(function(){
+
+    var newTodoText = $('input').val();
+
+    if(newTodoText.length > 0){
+      $.ajax(
+
+        {
+          url: 'http://157.230.17.132:3003/todos/',
+          method: 'POST',
+          data: {
+            text: newTodoText
+          },
+          success: function(dataRes){
+            getTodoTodo();
+
+          },
+          error: function(){
+            alert('La tua richiesta non ha portato alcun risultato')
+          }
+        }
+      );
+    }else {
+      alert('Non lasciare il campo vuoto')
+    }
+
+  });
+
+  $(document).on('click', '.delete'
+    function (){
+      $.ajax(
+        {
+          url: 'http://157.230.17.132:3003/todos/',
+          method: 'DELETE',
+
+        }
+      );
+
+
+  });
+  // Funzione di lettura e stampa dei dati in seguito alla chiamata api
+  function getTodoTodo(){
+    $('#todo-list').html('');
     $.ajax(
 
       {
@@ -11,28 +57,21 @@ $(document).ready(function(){
 
             var source = $("#list-template").html();
             var template = Handlebars.compile(source);
+
             for (var i = 0; i < dataRes.length; i++) {
               var todoItem = dataRes[i];
+
               var html = template(todoItem);
               $('#todo-list').append(html);
             }
           }
         },
         error: function(){
-          alert('La tua richiesta non ha portato alcun risultato');
+          alert('Ops, Errore');
         }
 
       }
     );
   };
-
-  $('#add-todo').click(function(){
-
-    var newTodoText = $('input').val();
-
-    
-
-
-  });
 
 });
